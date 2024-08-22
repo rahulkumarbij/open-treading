9b540f747c8ca3db28d9f75387096cf3270c77b0
rahulkumarbij <rahulkumarbijarniya123@gmail.com>
2024-08-22 14:03:02 +0530

    finished project

---
+++ src/Api/api.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+const DEPLOYED='https://e-commerce-server-production-0873.up.railway.app'
+const LOCALHOST='http://localhost:5454'
+
+export const API_BASE_URL = LOCALHOST
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers:{
+    "Content-Type":'application/json'
+  }
+});
+
+const token = localStorage.getItem('jwt');
+
+api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+api.defaults.headers.post['Content-Type'] = 'application/json';
+
+export default api;
